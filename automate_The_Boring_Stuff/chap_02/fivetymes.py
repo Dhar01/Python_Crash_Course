96cfca868a4642ac4135cc3364e253a64d6efd47
Loknath Dhar <loknath-dhar@pm.me>
2020-03-20 20:33:45 +0600

    ATBSP Chapter 02: Done

---
+++ automate_The_Boring_Stuff/chap_02/fivetymes.py
@@ -0,0 +1,15 @@
+# FOR loop
+
+print('My name is')
+for i in range(5):
+    print('Jimmy Five Times (' + str(i) + ')')
+
+
+# equivalent WHILE loop
+
+print('My name is')
+i = 0
+
+while i < 5:
+    print('Jimmy Five Times (' + str(i) + ')')
+    i = i+1
--- automate_The_Boring_Stuff/chap_02/printRandom.py
+++ automate_The_Boring_Stuff/chap_02/printRandom.py
@@ -1,3 +1,6 @@
-import random
+#import random
+from random import *
+
 for i in range(5):
-    print(random.randint(1, 10))
+#    print(random.randint(1, 10))
+    print(randint(1, 10))
