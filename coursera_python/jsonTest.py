2e0e654dcde4a1759686d20c189734722323619e
Loki <loknathdhar66@gmail.com>
2020-05-27 19:29:28 +0600

    update

---
+++ coursera_python/jsonTest.py
@@ -0,0 +1,15 @@
+import json
+data = '''{
+    "name" : "Chuck",
+    "phone" : {
+        "type" : "intl",
+        "number" : "+17343034456"
+    },
+    "email" : {
+        "hide" : "yes"
+    }
+}'''
+
+info = json.loads(data)
+print('Name:', info["name"])
+print('Hide:', info["email"]["hide"])
