874abe4f349cbba8970a4c7f987df434178c70a2
Loki <loknathdhar66@gmail.com>
2020-05-29 21:45:49 +0600

    update

--- coursera_python/geojsonTest01.py
+++ coursera_python/geojsonTest01.py
@@ -3,14 +3,11 @@ import json
 import ssl
 
 api_key = False
-# If you have a Google Places API key, enter it here
-# api_key = 'AIzaSy___IDByT70'
-# https://developers.google.com/maps/documentation/geocoding/intro
 
 if api_key is False:
     api_key = 42
     serviceurl = 'http://py4e-data.dr-chuck.net/json?'
-else :
+else:
     serviceurl = 'https://maps.googleapis.com/maps/api/geocode/json?'
 
 # Ignore SSL certificate errors
@@ -20,11 +17,14 @@ ctx.verify_mode = ssl.CERT_NONE
 
 while True:
     address = input('Enter location: ')
-    if len(address) < 1: break
+    if len(address) < 1: 
+        break
 
     parms = dict()
     parms['address'] = address
-    if api_key is not False: parms['key'] = api_key
+    if api_key is not False: 
+        parms['key'] = api_key
+
     url = serviceurl + urllib.parse.urlencode(parms)
 
     print('Retrieving', url)
